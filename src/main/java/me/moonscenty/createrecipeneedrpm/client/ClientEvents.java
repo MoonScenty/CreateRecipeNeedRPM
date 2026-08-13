38f97b1e8408457d2f30e2c1f3d5a31f6a6dff62
MoonScenty <silvia_s@naver.com>
2026-08-13 17:06:05 +0900

    millstone이 보이도록 변경

---
+++ src/main/java/me/moonscenty/createrecipeneedrpm/client/ClientEvents.java
@@ -0,0 +1,48 @@
+package me.moonscenty.createrecipeneedrpm.client;
+
+import com.simibubi.create.AllPartialModels;
+import com.simibubi.create.content.kinetics.base.SingleAxisRotatingVisual;
+import com.simibubi.create.content.kinetics.millstone.MillstoneRenderer;
+import dev.engine_room.flywheel.lib.visualization.SimpleBlockEntityVisualizer;
+import me.moonscenty.createrecipeneedrpm.CreateRecipeNeedRPM;
+import me.moonscenty.createrecipeneedrpm.registry.ModBlockEntities;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+
+@EventBusSubscriber(
+        modid = CreateRecipeNeedRPM.MOD_ID,
+        value = Dist.CLIENT
+)
+public final class ClientEvents {
+
+    private ClientEvents() {
+    }
+
+    @SubscribeEvent
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(
+                ModBlockEntities.RPM_MILLSTONE.get(),
+                context -> (BlockEntityRenderer) new MillstoneRenderer(context)
+        );
+    }
+
+    @SubscribeEvent
+    public static void clientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            SimpleBlockEntityVisualizer
+                    .builder(ModBlockEntities.RPM_MILLSTONE.get())
+                    .factory(
+                            SingleAxisRotatingVisual.of(
+                                    AllPartialModels.MILLSTONE_COG
+                            )
+                    )
+                    .skipVanillaRender(blockEntity -> true)
+                    .apply();
+        });
+    }
+}
